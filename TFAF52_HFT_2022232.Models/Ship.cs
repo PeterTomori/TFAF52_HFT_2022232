c89c4aec0738ab1445f988a02ecc57b3973c0f38
Peter9@stud.uni-obuda.hu <Peter9@stud.uni-obuda.hu>
2023-05-15 00:01:27 +0200

    Ship class created.

---
+++ TFAF52_HFT_2022232.Models/Ship.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFAF52_HFT_2022232.Models
+{
+    //Id = 1, ShipName = "Mon Calamari Cruiser", ShipType = "Capital Ship", CompanyId = 1}
+    public class Ship
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string ShipName { get; set; }
+        public string ShipType { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
