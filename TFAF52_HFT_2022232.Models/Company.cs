e2ad758c8f3ae25748568d534a8eec7349d35067
Peter9@stud.uni-obuda.hu <Peter9@stud.uni-obuda.hu>
2023-05-14 23:59:59 +0200

    Company class created.

---
+++ TFAF52_HFT_2022232.Models/Company.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFAF52_HFT_2022232.Models
+{
+    //{Id = 1, CompanyName = "Mon Calamari Shipyards", Faction = "Rebel Alliance" }
+    public class Company
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [StringLength(30)]
+        public string CompanyName { get; set; }
+        public string Faction { get; set; }
+    }
+}
