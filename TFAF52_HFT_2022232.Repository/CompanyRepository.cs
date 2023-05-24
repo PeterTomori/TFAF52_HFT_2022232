ba4cbd292bd14b0da68bd448b43e274022407311
Peter9@stud.uni-obuda.hu <Peter9@stud.uni-obuda.hu>
2023-05-24 16:56:15 +0200

    CompanyRepository class created.

---
+++ TFAF52_HFT_2022232.Repository/CompanyRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TFAF52_HFT_2022232.Models;
+
+namespace TFAF52_HFT_2022232.Repository
+{
+    public class CompanyRepository : Repository<Company>, IRepository<Company>
+    {
+        public CompanyRepository(ShipDbContext context) : base(context)
+        {
+        }
+
+        public override Company Read(int id)
+        {
+            return context.Companies.FirstOrDefault(t => t.CompanyId == id);
+        }
+
+        public override void Update(Company item)
+        {
+            var old = Read(item.CompanyId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            context.SaveChanges();
+        }
+    }
+}
