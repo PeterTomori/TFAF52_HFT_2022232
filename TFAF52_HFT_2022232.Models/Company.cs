e3032a178d93e81238a55da035f68f03723d0fbc
Peter9@stud.uni-obuda.hu <Peter9@stud.uni-obuda.hu>
2023-05-24 12:47:55 +0200

    JsonIgnore attribue added.

--- TFAF52_HFT_2022232.Models/Company.cs
+++ TFAF52_HFT_2022232.Models/Company.cs
@@ -4,18 +4,31 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace TFAF52_HFT_2022232.Models
 {
-    //{Id = 1, CompanyName = "Mon Calamari Shipyards", Faction = "Rebel Alliance" }
     public class Company
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int CompanyId { get; set; }
-        [StringLength(30)]
         public string CompanyName { get; set; }
         public string Faction { get; set; }
+
+        [NotMapped]
+        [JsonIgnore]
+        public virtual ICollection<Ship> Ships { get; set; }
+
+        [NotMapped]
+        [JsonIgnore]
+        public virtual ICollection<Planet> Planets { get; set; }
+
+        public Company()
+        {
+            Ships = new HashSet<Ship>();
+            Planets = new HashSet<Planet>();
+        }
     }
 }
