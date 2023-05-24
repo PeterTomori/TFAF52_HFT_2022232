7bdcd20d216514c9550731b4ffe69fb13f1dd372
Peter9@stud.uni-obuda.hu <Peter9@stud.uni-obuda.hu>
2023-05-24 16:56:34 +0200

    PlanetRepository class created.

---
+++ TFAF52_HFT_2022232.Repository/PlanetRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TFAF52_HFT_2022232.Models;
+
+namespace TFAF52_HFT_2022232.Repository
+{
+    public class PlanetRepository : Repository<Planet>, IRepository<Planet>
+    {
+        public PlanetRepository(ShipDbContext context) : base(context)
+        {
+        }
+
+        public override Planet Read(int id)
+        {
+            return context.Planets.FirstOrDefault(t => t.PlanetId == id);
+        }
+
+        public override void Update(Planet item)
+        {
+            var old = Read(item.PlanetId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            context.SaveChanges();
+        }
+    }
+}
