8332441f42662f23944e1fc0535180380a603193
Peter9@stud.uni-obuda.hu <Peter9@stud.uni-obuda.hu>
2023-06-01 21:51:23 +0200

    Files modified.

--- TFAF52_HFT_2022232.Endpoint/Startup.cs
+++ TFAF52_HFT_2022232.Endpoint/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
@@ -17,8 +18,13 @@ namespace TFAF52_HFT_2022232.Endpoint
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<ShipDbContext>();
@@ -44,6 +50,8 @@ namespace TFAF52_HFT_2022232.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ShipDbApp.Endpoint v1"));
             }
 
             app.UseExceptionHandler(c => c.Run(async context =>
@@ -57,12 +65,11 @@ namespace TFAF52_HFT_2022232.Endpoint
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
