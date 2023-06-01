28040d149f444f8ea0ee737d8beca08aaf76cac5
Peter9@stud.uni-obuda.hu <Peter9@stud.uni-obuda.hu>
2023-06-01 21:34:00 +0200

    Tester classes created.

---
+++ TFAF52_HFT_2022232.Test/CompanyTester.cs
@@ -0,0 +1,66 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TFAF52_HFT_2022232.Logic;
+using TFAF52_HFT_2022232.Models;
+using TFAF52_HFT_2022232.Repository;
+
+namespace TFAF52_HFT_2022232.Test
+{
+    [TestFixture]
+    public class CompanyTester
+    {
+        CompanyLogic cl;
+        Mock<IRepository<Company>> mockCompanyRepository;
+
+        [SetUp]
+        public void Init()
+        {
+            mockCompanyRepository = new Mock<IRepository<Company>>();
+
+            Company company1 = new Company(1, "CompanyA", "AAA");
+            Company company2 = new Company(2, "CompanyB", "AAA");
+
+            Ship ship1 = new Ship(1, "ShipA", "Frigate", 1);
+            Ship ship2 = new Ship(2, "ShipB", "Starfighter", 1);
+
+            Planet planet = new Planet(1, "PlanetA", 1);
+
+            company1.Ships.Add(ship1);
+            company1.Ships.Add(ship2);
+
+            company1.Planets.Add(planet);
+
+            mockCompanyRepository.Setup(c => c.ReadAll()).Returns(new List<Company>()
+            {
+                company1, company2
+            }.AsQueryable());
+
+            cl = new CompanyLogic(mockCompanyRepository.Object);
+        }
+
+        [Test]
+        public void OwnedByCompanyTester()
+        {
+            var result = cl.OwnedByCompany("CompanyA").ToArray();
+
+            Assert.That(result[0].PlanetId == 1 &&
+                        result[0].PlanetName == "PlanetA");
+        }
+
+        [Test]
+        public void ShipOfFactionsTester()
+        {
+            var result = cl.ShipOfFactions("AAA").ToArray();
+
+            Assert.That(result[0].ShipId == 1 &&
+                        result[0].ShipName == "ShipA");
+            Assert.That(result[1].ShipId == 2 &&
+                        result[1].ShipName == "ShipB");
+        }
+    }
+}
--- TFAF52_HFT_2022232.Test/Tester.cs
+++ TFAF52_HFT_2022232.Test/Tester.cs
@@ -1,6 +1,8 @@
 ﻿using Moq;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using TFAF52_HFT_2022232.Logic;
 using TFAF52_HFT_2022232.Models;
 using TFAF52_HFT_2022232.Repository;
@@ -19,7 +21,126 @@ namespace TFAF52_HFT_2022232.Test
         CompanyLogic cl;
         Mock<IRepository<Company>> mockCompanyRepository;
 
+        [SetUp]
+        public void Init()
+        {
+            mockShipRepository = new Mock<IRepository<Ship>>();
+            mockCompanyRepository = new Mock<IRepository<Company>>();
+            mockPlanetRepository = new Mock<IRepository<Planet>>();
 
+            List<Company> companies = new List<Company>()
+            {
+                new Company(){ CompanyId = 1, CompanyName = "CompanyA", Faction = "ABC"},
+                new Company(){ CompanyId = 2, CompanyName = "CompanyB", Faction = "ABC"},
+                new Company(){ CompanyId = 3, CompanyName = "CompanyC", Faction = "AAA"}
+            };
 
+            mockCompanyRepository.Setup(c => c.ReadAll()).Returns(companies.AsQueryable());
+
+            List<Ship> ships = new List<Ship>()
+            {
+                new Ship(){ ShipId = 1, ShipName = "ShipA", Company = companies[0]},
+                new Ship(){ ShipId = 2, ShipName = "ShipB", Company = companies[1]},
+                new Ship(){ ShipId = 3, ShipName = "ShipC", Company = companies[2]}
+            };
+
+            mockShipRepository.Setup(s => s.ReadAll()).Returns(ships.AsQueryable());
+
+            List<Planet> planets = new List<Planet>()
+            {
+                new Planet(){ PlanetId = 1, PlanetName = "PlanetA",  Company = companies[0]},
+                new Planet(){ PlanetId = 2, PlanetName = "PlanetB",  Company = companies[1]},
+                new Planet(){ PlanetId = 3, PlanetName = "PlanetC",  Company = companies[2]}
+            };
+
+            mockPlanetRepository.Setup(p => p.ReadAll()).Returns(planets.AsQueryable());
+
+            pl = new PlanetLogic(mockPlanetRepository.Object);
+
+            cl = new CompanyLogic(mockCompanyRepository.Object);
+
+            sl = new ShipLogic(mockShipRepository.Object);
+        }
+
+        [Test]
+        public void CreateShipTestWithCorrectTitle()
+        {
+            var ship = new Ship() { ShipName = "ABCD" };
+            sl.Create(ship);
+            mockShipRepository.Verify(r => r.Create(ship), Times.Once);
+        }
+
+        [Test]
+        public void CreateShipTestWithIncorrectTitle()
+        {
+            var ship = new Ship() { ShipName = "AB" };
+
+            try
+            {
+                sl.Create(ship);
+            }
+            catch
+            {
+                
+            }
+
+            mockShipRepository.Verify(s => s.Create(ship), Times.Never);
+        }
+
+        [Test]
+        public void CreatePlanetTest()
+        {
+            var planet = new Planet() { PlanetName = "Naboo" };
+            pl.Create(planet);
+            mockPlanetRepository.Verify(p => p.Create(planet), Times.Once);
+        }
+
+        [Test]
+        public void CreateCompanyTest()
+        {
+            var company = new Company { CompanyName = "KFT" };
+            cl.Create(company);
+            mockCompanyRepository.Verify(c => c.Create(company), Times.Once);
+        }
+
+        [Test]
+        public void ShipManufacturersTester()
+        {
+            var result = sl.ShipManufacturers("ShipA").ToArray();
+
+            Assert.That(result[0].CompanyId == 1 &&
+                        result[0].CompanyName == "CompanyA" &&
+                        result[0].Faction == "ABC");
+        }
+
+        [Test]
+        public void ShipFactionsTester_WithCorrectResult()
+        {
+            var result = sl.ShipFactions().ToArray();
+            var expected = new List<FactionCounted>() { new FactionCounted() { Faction = "ABC", ShipCount = 2 } };
+
+            Assert.That(result[0].Faction, Is.EqualTo(expected[0].Faction));
+            Assert.That(result[0].ShipCount, Is.EqualTo(expected[0].ShipCount));
+        }
+
+        [Test]
+        public void ShipFactionsTester_WithIncorrectResult()
+        {
+            var result = sl.ShipFactions().ToArray();
+            var expected = new List<FactionCounted>() { new FactionCounted() { Faction = "ABC", ShipCount = 3 } };
+
+            //Assert.That(result[0].Faction != expected[0].Faction);
+            Assert.That(result[0].ShipCount != expected[0].ShipCount);
+        }
+
+        [Test]
+        public void OwnerOfPlanetTester()
+        {
+            var result = pl.OwnerOfPlanet("PlanetA").ToArray();
+
+            Assert.That(result[0].CompanyId == 1 &&
+                        result[0].CompanyName == "CompanyA" &&
+                        result[0].Faction == "ABC");
+        }
     }
 }
