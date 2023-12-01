f332699b34a013451cc8f3017a0003c658e22ff6
Peter9@stud.uni-obuda.hu <Peter9@stud.uni-obuda.hu>
2023-12-01 18:17:47 +0100

    Commands expanded.

--- TFAF52_HFT_2022232.WPFClient/MainWindowViewModel.cs
+++ TFAF52_HFT_2022232.WPFClient/MainWindowViewModel.cs
@@ -16,6 +16,10 @@ namespace TFAF52_HFT_2022232.WPFClient
     {
         public RestCollection<Company> Companies { get; set; }
 
+        public RestCollection<Planet> Planets { get; set; }
+
+        public RestCollection<Ship> Ships { get; set; }
+
         private Company selectedCompany;
 
         public Company SelectedCompany
@@ -28,6 +32,28 @@ namespace TFAF52_HFT_2022232.WPFClient
                     selectedCompany = new Company()
                     {
                         CompanyName = value.CompanyName,
+                        CompanyId = value.CompanyId,
+                        Faction = value.Faction
+                    };
+                    OnPropertyChanged();
+                    (DeleteCompanyCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+            }
+        }
+
+        private Planet selectedPlanet;
+
+        public Planet SelectedPlanet
+        {
+            get { return selectedPlanet; }
+            set
+            {
+                if (value != null)
+                {
+                    selectedPlanet = new Planet()
+                    {
+                        PlanetName = value.PlanetName,
+                        PlanetId = value.PlanetId,
                         CompanyId = value.CompanyId
                     };
                     OnPropertyChanged();
@@ -36,11 +62,49 @@ namespace TFAF52_HFT_2022232.WPFClient
             }
         }
 
+        private Ship selectedShip;
+
+        public Ship SelectedShip
+        {
+            get { return selectedShip; }
+            set
+            {
+                if (value != null)
+                {
+                    selectedShip = new Ship()
+                    {
+                        ShipName = value.ShipName,
+                        ShipId = value.ShipId,
+                        ShipType = value.ShipType,
+                        CompanyId = value.CompanyId
+                    };
+                    OnPropertyChanged();
+                    (DeleteCompanyCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+            }
+        }
+
+        #region Commands
+
         public ICommand CreateCompanyCommand { get; set; }
 
         public ICommand DeleteCompanyCommand { get; set; }
 
         public ICommand UpdateCompanyCommand { get; set; }
+
+        public ICommand CreateShipCommand { get; set; }
+
+        public ICommand DeleteShipCommand { get; set; }
+
+        public ICommand UpdateShipCommand { get; set; }
+
+        public ICommand CreatePlanetCommand { get; set; }
+
+        public ICommand DeletePlanetCommand { get; set; }
+
+        public ICommand UpdatePlanetCommand { get; set; }
+
+        #endregion
 
         public static bool IsInDesignMode
         {
@@ -56,6 +120,11 @@ namespace TFAF52_HFT_2022232.WPFClient
             if (!IsInDesignMode)
             {
                 Companies = new RestCollection<Company>("http://localhost:27110/", "company", "hub");
+                Planets = new RestCollection<Planet>("http://localhost:27110/", "planet", "hub");
+                Ships = new RestCollection<Ship>("http://localhost:27110/", "ship", "hub");
+
+                // Company Commands
+
                 CreateCompanyCommand = new RelayCommand(() =>
                 {
                     Companies.Add(new Company()
@@ -78,6 +147,56 @@ namespace TFAF52_HFT_2022232.WPFClient
                     return SelectedCompany != null;
                 });
                 SelectedCompany = new Company();
+
+                // Planet Commands
+
+                CreatePlanetCommand = new RelayCommand(() =>
+                {
+                    Planets.Add(new Planet()
+                    {
+                        PlanetName = SelectedPlanet.PlanetName
+                    });
+                });
+
+                UpdatePlanetCommand = new RelayCommand(() =>
+                {
+                    Planets.Update(SelectedPlanet);
+                });
+
+                DeletePlanetCommand = new RelayCommand(() =>
+                {
+                    Planets.Delete(SelectedPlanet.PlanetId);
+                },
+                () =>
+                {
+                    return SelectedPlanet != null;
+                });
+                SelectedPlanet = new Planet();
+
+                // Ship Commands
+
+                CreateShipCommand = new RelayCommand(() =>
+                {
+                    Ships.Add(new Ship()
+                    {
+                        ShipName = SelectedShip.ShipName
+                    });
+                });
+
+                UpdateShipCommand = new RelayCommand(() =>
+                {
+                    Ships.Update(SelectedShip);
+                });
+
+                DeleteShipCommand = new RelayCommand(() =>
+                {
+                    Ships.Delete(SelectedShip.ShipId);
+                },
+                () =>
+                {
+                    return SelectedShip != null;
+                });
+                SelectedShip = new Ship();
             }
         }
     }
